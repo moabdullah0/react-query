2ed78768e4bde8fc26f68692b0f374a4855b1486
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2025-02-02 10:16:59 +0300

    add infinit query

--- src/PostList.jsx
+++ src/PostList.jsx
@@ -1,40 +1,45 @@
-import { useState } from "react";
+import React from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
   const pageSize = 10;
-  const [page, setPage] = useState(1); 
 
-  const { data, error, isLoading } = usePosts({ page, pageSize });
-
-  const hasNextPage = data && data.length === pageSize; 
+  const {
+    data,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = usePosts({ pageSize });
 
   return (
     <div>
       <div>
         {isLoading && <p>Loading posts...</p>}
         {error && <p>Error fetching posts!</p>}
-        {data?.map((post) => (
-          <li key={post.id}>{post.title}</li>
+
+        {/* Render posts if data exists */}
+        {data?.pages.map((page, pageIndex) => (
+          <React.Fragment key={pageIndex}>
+            {page.map((post) => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </React.Fragment>
         ))}
       </div>
 
+      {/* "Load More" button */}
       <div className="flex mt-4">
-        <button
-          className="bg-blue-500 py-2 px-5 text-white rounded-xl"
-          onClick={() => setPage(page - 1)}
-          disabled={page === 1}
-        >
-          Previous
-        </button>
-        
-        <button
-          className="bg-blue-500 py-2 px-5 text-white mx-2 rounded-xl"
-          onClick={() => setPage(page + 1)}
-          disabled={!hasNextPage } // Disable if no next page or fetching
-        >
-          Next
-        </button>
+        {hasNextPage && (
+          <button
+            className="bg-blue-500 py-2 px-5 text-white mx-2 rounded-xl"
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            {isFetchingNextPage ? "Loading..." : "Load More"}
+          </button>
+        )}
       </div>
     </div>
   );
--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -1,28 +1,46 @@
 import apiClient from "../Services/Services";
-import { useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
 
-const usePosts = ({ page, pageSize }) => {
-
-  const fetchPosts = async () => {
-    const res = await apiClient.get("/posts", {
-      params: {
-        _start: (page - 1) * pageSize, 
-        _limit: pageSize,
-      },
-    });
+const usePosts = ({ pageSize }) => {
+  const fetchPosts = async ({ pageParam = 1 }) => {
+    const res = await apiClient
+      .get("/posts", {
+        params: {
+          _start: (pageParam - 1) * pageSize,
+          _limit: pageSize,
+        },
+      })
+     
     return res.data;
   };
 
- 
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["posts", page, pageSize], 
-    queryFn: fetchPosts, 
+  const {
+    data,
+    error,
+    isLoading,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+    getNextPageParam: (lastPage, allpages) => {
+      //1 =>2
+      return lastPage.length > 0 ? allpages.length + 1 : undefined;
+    },
   });
 
   return {
     data,
     error,
     isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
   };
 };
 
